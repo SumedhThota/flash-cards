1c8ae5f8058c68ac39294f8794d2b2a4354c9f79
SumedhThota <sumedharya569@gmail.com>
2024-07-10 15:58:18 -0400

    Initial commit

---
+++ src/FlashCards.js
@@ -0,0 +1,11 @@
+export default function FlashCards() {
+    return <div className ="flashcards">
+        <div>A</div>
+        <div>A</div>
+        <div>A</div>
+        <div>A</div>
+        <div>A</div>
+        <div>A</div>
+    </div>;
+  }
+  
